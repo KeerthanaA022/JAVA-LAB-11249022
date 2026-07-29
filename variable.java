4bdd58b5a101cf2166949356bb76dd3e8daff8ae
KeerthanaA022 <11249A022@kanchiuniv.ac.in>
2026-07-29 11:53:27 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,13 @@
+public class variable {
+int instance var = 10;
+static string static var = "I am static";
+public void show variables ()
+{
+    system out print ln ("Instance variable :"+ instance var);
+    system out print ln ("static variable :"+ static var);
+    system out print ln ("Local variable :"+ local var);
+}
+public static void main (string [] args)
+{
+    variablea Demo obj 1 = new variable   
+}
